6fee4d524e2bebc9572814695321367a24bf266d
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-01 11:13:12 +0200

    login_form

--- login.php
+++ login.php
@@ -13,9 +13,55 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/reset.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/dashboard.css">
     <title>Login</title>
 </head>
 <body>
+
+<div class="limiter">
+		<div class="container">
+			<div class="wrap">
+				<form class="form">
+					<span class="form--title">
+						Login
+					</span>
+
+					
+					<div class="form--input">
+						<input class="input" type="text" name="username" placeholder="Username">
+						<span class="input--focus"></span>
+					</div>
+					
+					
+					<div class="form--input">
+						<input class="input" type="password" name="pass" placeholder="Password">
+						<span class="input--focus"></span>
+					</div>
+					
+					<div class="form--check">
+						<div class="form--checkbox">
+							<input class="input--checkbox" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label--checkbox" for="ckb1">
+								Remember me
+							</label>
+						</div>
+
+						<div>
+							<a href="#" class="txt1">
+								Forgot?
+							</a>
+						</div>
+					</div>
+					<div class="form--btn">
+						<button >
+							Login
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
     
 </body>
 </html>
