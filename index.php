aec052c13f6e97c08e46f5f6a0048e84e87acaf8
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-05-08 20:32:41 +0200

    fixed scrollbar bug

--- index.php
+++ index.php
@@ -110,5 +110,13 @@
         </form>
     </footer>
     <script src="js/mobileNavigation.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script>
+        $.each($('*'), function() {
+            if ($(this).width() > $('body').width()) {
+                console.log("Wide Element: ", $(this), "Width: ", $(this).width());
+            }
+        });
+    </script>
 </body>
 </html>
