552f6b7dc746e102697f5ebf75941bfbab228cb0
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-09 20:56:09 +0200

    Testje

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,17 @@
+
+<?php
+
+$dataPoints = array(
+    array('label' => 'Limit', 'symbol' => 'O', 'y' => 60),
+    array('label' => 'Today', 'symbol' => 'Si', 'y' => 40),
+);
+
+?>
+
+
+
+
+
 <?php
 
     //Connectie klasses
@@ -26,7 +40,9 @@
   <div class='column'>
     <div class='item'></div>
     <div class='item'></div>
-    <div class='item'></div>
+    <div class='item'>
+      <div id="chartContainer" style="height: 370px; width: 100%;"> </div>
+    </div>
   </div>
   <div class='column'>
     <div class='item'></div>
@@ -45,6 +61,28 @@
 
 <script src="js/webNavigation.js"></script>
     
-
+<script>
+window.onload = function() {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light2",
+	animationEnabled: true,
+	title: {
+		text: "Average Composition of Magma"
+	},
+	data: [{
+		type: "doughnut",
+		indexLabel: "{symbol} - {y}",
+		yValueFormatString: "#,##0.0\"%\"",
+		showInLegend: true,
+		legendText: "{label} : {y}",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>
