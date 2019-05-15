e3f969e3f929f0a705aa8413ce7dbd86b56a628a
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-15 11:48:21 +0200

    update sql

--- weslezp319_drippeo.sql
+++ weslezp319_drippeo.sql
@@ -1,26 +1,64 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Gegenereerd op: 05 mei 2019 om 21:35
--- Serverversie: 10.1.38-MariaDB
--- PHP-versie: 7.3.2
+-- Host: localhost:3306
+-- Gegenereerd op: 15 mei 2019 om 09:47
+-- Serverversie: 5.7.23
+-- PHP-versie: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `weslezp319_drippeo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `productcode`
+--
+
+CREATE TABLE `productcode` (
+  `id` int(11) NOT NULL,
+  `productCode` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `productcode`
+--
+
+INSERT INTO `productcode` (`id`, `productCode`) VALUES
+(1, 'a1b2c3d4e5'),
+(2, 'b2c3d4e5f6'),
+(3, 'c3d4e5f6g7'),
+(4, 'd4e5f6g7h8'),
+(5, 'e5f6g7h8i9');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `product_settings`
+--
+
+CREATE TABLE `product_settings` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `residents` int(11) NOT NULL,
+  `showers` int(11) NOT NULL,
+  `baths` int(11) NOT NULL,
+  `toilets` int(11) NOT NULL,
+  `sinks` int(11) NOT NULL,
+  `outside_tap` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `product_settings`
+--
+
+INSERT INTO `product_settings` (`id`, `user_id`, `residents`, `showers`, `baths`, `toilets`, `sinks`, `outside_tap`) VALUES
+(1, 19, 3, 1, 1, 4, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -39,28 +77,35 @@ CREATE TABLE `users` (
   `city` varchar(255) NOT NULL,
   `postalCode` int(11) NOT NULL,
   `phone` int(11) NOT NULL,
-  `verbruikId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `productcode_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `firstName`, `lastName`, `street`, `number`, `city`, `postalCode`, `phone`, `verbruikId`) VALUES
-(1, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(2, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(3, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(4, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(5, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(6, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(7, 'test@test.com', 'qwerty', 'test', 'tester', 'ergens', '45', 'Antwerpen', 2000, 475689951, 0),
-(8, 'wwke@test.com', '$2y$10$o7PCG8FnOXDTXBPlugRpdOKUmfcOdtJsllhBhAyR6L839QVJB8Y5K', 'wes', 'ke', 'ergens', '54', 'Berchem', 2600, 412365478, 0),
-(9, 'nieuw@test.com', '$2y$13$oSyPqHTSigGTpCUfeIfx5eUCrM8/DkF9GRoX08sbhk4mZkJr2.MRS', 'nie', 'uw', 'Anders', '32', 'Deurne', 2100, 477951753, 0),
-(10, 'test@tester.be', '$2y$13$8y2rz0wDAScHsUstMBDPF.dGobcOCymw/DBsZ3QqrVThX64IrVolW', 'test', 'tester', 'teststraat', '404', 'Debug', 4040, 479123654, 0);
+INSERT INTO `users` (`id`, `email`, `password`, `firstName`, `lastName`, `street`, `number`, `city`, `postalCode`, `phone`, `productcode_id`) VALUES
+(1, 'weske@test.com', '$2y$13$BF8Go8PBU6mGGzPXM2CW.Ojk5VJx5u6LGA1gNRPrLQHyOrC1mtAdu', 'wes', 'ke', 'ergens', '45', 'antwerpen', 2000, 475951357, 1),
+(7, 'nieuw@test.com', '$2y$13$HsZbcl1Y9AJbqxUtW1iKC.ICaoaRug3ryfFKYWwFNkC.8MmP9evau', 'Mike', 'kerel', 'wasstraat', '123', 'Antwerpen', 2000, 495123456, 2),
+(14, 'weske@iets.com', '$2y$13$E.mTfuTKussZ.hLQGUDgkutwbQH.UYLGjQ.XnMGZosWFX/scdZX5a', 'wasefdgh', 'asfdgfhmhj', 'asdfghm', '4562', 'sfdgfgh', 7856, 2147483647, 4),
+(15, 'ietske@nietske.be', '$2y$13$tPbpYzHdQXFE1fuI9ByQXexN5mhBgQd19blV1iQ.kPpxRtko/u5ly', 'ikke', 'niet', 'eenStraat', '99', 'Ekeren', 2180, 495365784, 5),
+(22, 'elke.borreij@gmail.com', '$2y$13$kLY/MzKtm.npxBKnjhX5Mu/Gd/6TUSmbrDXEIg2WDv7dkbvvWCP0W', 'Elke', 'Borreij', 'Koning Albertstraat', '110', 'Mechelen', 2800, 498825380, 3);
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `productcode`
+--
+ALTER TABLE `productcode`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `product_settings`
+--
+ALTER TABLE `product_settings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `users`
@@ -73,12 +118,19 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT voor een tabel `productcode`
+--
+ALTER TABLE `productcode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT voor een tabel `product_settings`
+--
+ALTER TABLE `product_settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
